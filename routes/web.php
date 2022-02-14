14d3f07117b130e50105e31d051de1e9d790cbbc
fabinho070 <freitasfabiano08@gmail.com>
2022-02-14 20:11:23 -0300

    Ajustes

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,36 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PrincipalController@principal');
+
 Route::get('/sobre-nos', 'SobreNosController@sobrenos');
+
 Route::get('/contato', 'ContatoController@contato');
 
-Route::get(
-  '/contato/{nome}/{categoria_id?}',
-  function (
-    string $nome,
-    int $categoria_id  = 1
-  ) {
-    echo '<h3>Nome: ' . $nome . '</h3>';
-    echo '<h3>Categoria: ' . $categoria_id . '</h3>';
-  }
-)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
+
+//app
+
+Route::prefix('/app')->group(function () {
+  Route::get('/login', function () {
+    return 'Login';
+  });
+  Route::get('/clientes', function () {
+    return 'Clientes';
+  });
+  Route::get('/fornecedores', function () {
+    return 'Fornecedores';
+  });
+  Route::get('/produtos', function () {
+    return 'Produtos';
+  });
+});
+
+// Route::get(
+//   '/contato/{nome}/{categoria_id?}',
+//   function (
+//     string $nome,
+//     int $categoria_id  = 1
+//   ) {
+//     echo '<h3>Nome: ' . $nome . '</h3>';
+//     echo '<h3>Categoria: ' . $categoria_id . '</h3>';
+//   }
+// )->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
