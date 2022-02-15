cb1771f3d1924234027a037e25280632cb6e57ed
fabinho070 <freitasfabiano08@gmail.com>
2022-02-14 21:30:38 -0300

    Ajustes

--- routes/web.php
+++ routes/web.php
@@ -2,29 +2,36 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
 
-Route::get('/sobre-nos', 'SobreNosController@sobrenos');
+Route::get('/sobre-nos', 'SobreNosController@sobrenos')->name('site.sobrenos');
 
-Route::get('/contato', 'ContatoController@contato');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+
+Route::get('/login', function () {
+  return 'Login';
+})->name('app.login');
 
 
-//app
-
+//rotas privdadas
 Route::prefix('/app')->group(function () {
-  Route::get('/login', function () {
-    return 'Login';
-  });
   Route::get('/clientes', function () {
     return 'Clientes';
-  });
+  })->name('app.clientes');
   Route::get('/fornecedores', function () {
     return 'Fornecedores';
-  });
+  })->name('app.fornecedores');
   Route::get('/produtos', function () {
     return 'Produtos';
-  });
+  })->name('app.produtos');
 });
+
+Route::fallback(function () {
+  echo 'Erro ao localizar a página, clique <a href="' . route('site.index') .
+    '">aqui</a> para ir a página inicial';
+});
+
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
 
 // Route::get(
 //   '/contato/{nome}/{categoria_id?}',
