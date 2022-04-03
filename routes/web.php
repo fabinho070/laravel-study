ce6a9f1a005c036e1a0e74093d2b0f8a751c5470
fabinho070 <freitasfabiano08@gmail.com>
2022-04-03 17:05:16 -0300

    Ajustes

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@ Route::get('/login', function () {
 })->name('app.login');
 
 
-//rotas privdadas
 Route::prefix('/app')->group(function () {
   Route::get('/clientes', function () {
     return 'Clientes';
@@ -30,14 +29,3 @@ Route::fallback(function () {
 });
 
 Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
-
-// Route::get(
-//   '/contato/{nome}/{categoria_id?}',
-//   function (
-//     string $nome,
-//     int $categoria_id  = 1
-//   ) {
-//     echo '<h3>Nome: ' . $nome . '</h3>';
-//     echo '<h3>Categoria: ' . $categoria_id . '</h3>';
-//   }
-// )->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
