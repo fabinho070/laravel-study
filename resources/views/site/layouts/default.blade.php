889f4fb8727e8d9b0734be031d88fc7ea2a12730
fabinho070 <freitasfabiano08@gmail.com>
2022-04-03 16:40:42 -0300

    Ajustes

---
+++ resources/views/site/layouts/default.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Super Gestão - Sobre Nós</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    </head>
+      @yield('content')
+    <body>
+    </body>
+</html>
