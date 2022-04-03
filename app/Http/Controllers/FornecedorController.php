693106cb4b935f5a56f155e94283f01f880898dc
fabinho070 <freitasfabiano08@gmail.com>
2022-04-03 14:38:08 -0300

    Ajustes

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -8,7 +8,30 @@ class FornecedorController extends Controller
 {
     public function index()
     {
-        $fornecedores = ['Fornecedor', '1'];
+        $fornecedores = [
+            0 =>  ['nome' => 'Fornecedor 1',
+                   'status' => 'N',
+                   'cnpj' => 'xyz',
+                   'ddd' => '11',
+                   'Telefone' => '9999-9999'
+            ],
+            1 => ['nome' => 'Fornecedor 2',
+                  'status' => 'S',
+                  'cnpj' => null,
+                  'ddd' => '85',
+                  'Telefone' => '9999-9999'
+                 ],
+            2 => ['nome' => 'Fornecedor 3',
+                  'status' => 'S',
+                  'cnpj' => null,
+                  'ddd' => '48',
+                  'Telefone' => '9999-9999'
+                ],
+
+        ];
+
+        $fornecedores = [];
+
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,16 +1,18 @@
 <h1>Fornecedor</h1>
 
-{{-- estilo de comentário do blade --}}
+@isset($fornecedores)
 
-@php
+  @forelse ( $fornecedores as $indice => $fornecedor )
+      Fornecedor: {{ $fornecedor['nome'] }}
+    <br>
+      Status: {{ $fornecedor['status'] }}
+    <br>
+      CNPJ: {{ $fornecedor['cnpj'] ?? 'Dado não preenchido' }}
+    <br>
+      Telefone: {{($fornecedor['ddd']) ?? ''}} {{$fornecedor['telefone'] ?? ''}}
+    <hr>
+    @empty
+      Não existem fornecedores cadastrados.
+  @endforelse
 
-@endphp
-
-@dd($fornecedores)
-
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
-  <h3>Existem alguns fornecedores</h3>
-@elseif(count($fornecedores) > 10)
-  <h3>Existem vários fornecedores</h3>
-@else
-  <h3>Não existem fornecedores cadastrados</h3>
+@endisset
