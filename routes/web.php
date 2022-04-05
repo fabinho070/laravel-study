a117252100047345fef3b85113234766699f637a
fabinho070 <freitasfabiano08@gmail.com>
2022-04-04 22:19:09 -0300

    Add components

--- routes/web.php
+++ routes/web.php
@@ -3,29 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PrincipalController@principal')->name('site.index');
-
 Route::get('/sobre-nos', 'SobreNosController@sobrenos')->name('site.sobrenos');
-
+Route::post('/contato', 'ContatoController@contato')->name('site.contato');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
-
-Route::get('/login', function () {
-  return 'Login';
-})->name('app.login');
-
+Route::get('/login', function () { return 'Login'; })->name('app.login');
 
 Route::prefix('/app')->group(function () {
-  Route::get('/clientes', function () {
-    return 'Clientes';
-  })->name('app.clientes');
+  Route::get('/clientes', function () { return 'Clientes'; })->name('app.clientes');
   Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
-  Route::get('/produtos', function () {
-    return 'Produtos';
-  })->name('app.produtos');
+  Route::get('/produtos', function () { return 'Produtos'; })->name('app.produtos');
 });
 
-Route::fallback(function () {
-  echo 'Erro ao localizar a página, clique <a href="' . route('site.index') .
-    '">aqui</a> para ir a página inicial';
+Route::fallback(function () { echo
+    'Erro ao localizar a página, clique <a href="' . route('site.index') . '">aqui</a> para ir a página inicial';
 });
 
 Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
